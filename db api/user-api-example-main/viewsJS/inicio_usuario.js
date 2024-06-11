a239cf9f1416905bbafc89b259fe2ad87b84a1df
Kev1n011 <90216024+Kev1n011@users.noreply.github.com>
2024-06-10 21:53:21 -0700

    adad

--- db api/user-api-example-main/viewsJS/inicio_usuario.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('/get_user_email')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('userEmail').textContent = data.email;
-            document.getElementById('userEmailHidden').value = data.email; // Agrega el correo a un campo oculto
-        })
-        .catch(error => console.error('Error:', error));
-});
