35fc421d77635b4b80c52a46c94092cb1cb54d0a
Kev1n011 <90216024+Kev1n011@users.noreply.github.com>
2024-06-13 20:46:25 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,19 +1,32 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-    host: "viaduct.proxy.rlwy.net", 
+const pool = mysql.createPool({
+    host: "viaduct.proxy.rlwy.net",
     user: "root",
-    port: 16984, 
-    password: "DrPSlGiLCVkdLmmMXLOxEOibroFySriN", 
-    database: "railway" 
+    port: 16984,
+    password: "DrPSlGiLCVkdLmmMXLOxEOibroFySriN",
+    database: "railway",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected to MySQL database");
+pool.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('Database connection was closed.');
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR') {
+            console.error('Database has too many connections.');
+        }
+        if (err.code === 'ECONNREFUSED') {
+            console.error('Database connection was refused.');
+        }
+    }
+
+    if (connection) connection.release();
+    return;
 });
 
-
-
-module.exports = connection;
+module.exports = pool;
